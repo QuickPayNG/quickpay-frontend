6c9d8432a4449d27067efb259a68a2362ea18447
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-09-30 11:59:48 +0100

    implement Dashboard page and update routing

--- frontend-ts/src/router.tsx
+++ frontend-ts/src/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Dashboard from "./pages/Dashboard";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/dashboard/:userId",
+    element: <Dashboard />,
   },
   {
     path: "*",
